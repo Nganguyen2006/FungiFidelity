8fb7530876e5c037f187da92cd8d1a1d202f7891
Angelina Nguyen <angelinananguyen@gmail.com>
2023-02-19 10:30:16 -0500

    list mushrooms

--- components/redmush.js
+++ components/redmush.js
@@ -1,7 +1,7 @@
 Let score = 0 ; 
-Let sportsQuestionsList = ["How many people play on a volleyball court?", "How many miles is a 5k?", "Where does Wimbledon take place?", "Where did the 2016 olympics take place?", "How often does the Olympics take place?", "Who is the most decorated olympic champion?", "Who sang at the 2023 superbowl halftime show?" ]
-Let sportsAnswerList = []
-
+Let sportsQuestionsList = ["How many people play on a volleyball court?", "How many miles is a 5k?", "Where does Wimbledon take place?", "Where did the 2016 olympics take place?", "How often does the Olympics take place?", "Who is the most decorated olympic champion?", "Who sang at the 2023 superbowl halftime show?", "What is not a stroke in professional swim?", "What does MMA stand for", "What is Usain Bolt famous for?"];
+Let sportsCorrectAnswerList = ["6", "3.1 miles", "London", "Rio De Janiero", "Every 4 years", "Micheal Phelps", "Rihanna", "4", "Mixed Martial Arts", "100 meter dash"];
+Let musicQuestionList = ["What girl group was Beyonce in?", "What famous classical composer became completely deaf?", "What genre was Louis Armstrong famous for", "Who sang the song purple rain", "Drake famously said (Kiki do you ??..)", "How strings does a violin have?", ]
 onEvent ("sport.button", "click"){
 
 };
