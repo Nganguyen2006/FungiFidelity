db5b585a0f364274fa50f3e9bdac5a8af5ab6633
Angelina Nguyen <angelinananguyen@gmail.com>
2023-02-19 10:01:54 -0500

    commit /1

--- components/redmush.js
+++ components/redmush.js
@@ -1,5 +1,5 @@
 Let score = 0 ; 
-Let sportsQuestionsList = ["How many people play on a volleyball court?", "How many miles is a 5k?", "Where does Wimbledon take place?", "Where did the 2016 olympics take place?",  ]
+Let sportsQuestionsList = ["How many people play on a volleyball court?", "How many miles is a 5k?", "Where does Wimbledon take place?", "Where did the 2016 olympics take place?", "How often does the Olympics take place?", "Who is the most decorated olympic champion?", "Who sang at the 2023 superbowl halftime show?" ]
 Let sportsAnswerList = []
 
 onEvent ("sport.button", "click"){
